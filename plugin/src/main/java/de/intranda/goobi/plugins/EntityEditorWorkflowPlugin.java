f775e639130e1d5cb450479714bad19a5f6adb46
Robert Sehr <robert.sehr@intranda.com>
2021-11-11 15:52:43 +0100

    Layout

--- plugin/src/main/java/de/intranda/goobi/plugins/EntityEditorWorkflowPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/EntityEditorWorkflowPlugin.java
@@ -1,12 +1,15 @@
 package de.intranda.goobi.plugins;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.goobi.beans.Process;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IPlugin;
 import org.goobi.production.plugin.interfaces.IWorkflowPlugin;
 
+import de.intranda.goobi.plugins.model.BreadcrumbItem;
 import de.sub.goobi.helper.exceptions.DAOException;
 import de.sub.goobi.helper.exceptions.SwapException;
 import de.sub.goobi.persistence.managers.ProcessManager;
@@ -41,6 +44,16 @@ public class EntityEditorWorkflowPlugin implements IWorkflowPlugin, IPlugin {
     @Getter
     private Prefs prefs;
 
+    @Getter
+    private String entityType;
+
+    @Getter
+    private String entityColor;
+    @Getter
+    private String entityIcon;
+    @Getter
+    private List<BreadcrumbItem> breadcrumbList = new ArrayList<>();
+
     /**
      * Constructor
      */
@@ -57,6 +70,28 @@ public class EntityEditorWorkflowPlugin implements IWorkflowPlugin, IPlugin {
 
             currentFileformat = new MetsMods(prefs);
             currentFileformat.read(currentProcess.getMetadataFilePath());
+
+            BreadcrumbItem root = new BreadcrumbItem("dashboard", "Dashboard", 0, "#ccc", null);
+            breadcrumbList.add(root);
+
+            BreadcrumbItem item = new BreadcrumbItem("person", "John Doe", currentProcess.getId(), "#df07b9", "fa-user");
+            breadcrumbList.add(item);
+
+            BreadcrumbItem item2 = new BreadcrumbItem("award", "Darwin Award", currentProcess.getId(), "#05b8cd", "fa-trophy");
+            breadcrumbList.add(item2);
+
+            BreadcrumbItem item3 = new BreadcrumbItem("work", "Mona Lisa", currentProcess.getId(), "#900688", "fa-picture-o");
+            breadcrumbList.add(item3);
+
+            BreadcrumbItem item4 = new BreadcrumbItem("event", "World Cup", currentProcess.getId(), "#19b609", "fa-calendar");
+            breadcrumbList.add(item4);
+
+            BreadcrumbItem item5 = new BreadcrumbItem("agency", "intranda", currentProcess.getId(), "#e81c0c", "fa-university");
+            breadcrumbList.add(item5);
+
+            entityType = "person";
+            entityColor = "#df07b9";
+            entityIcon = "fa-user";
 
         } catch (UGHException | IOException | InterruptedException | SwapException | DAOException e) {
             log.error(e);
---
+++ plugin/src/main/java/de/intranda/goobi/plugins/model/BreadcrumbItem.java
@@ -0,0 +1,28 @@
+package de.intranda.goobi.plugins.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BreadcrumbItem {
+
+    private String entityType;
+    private String entityName;
+    private int processId;
+
+    // person: #df07b9
+    // agency: #e81c0c
+    // work: #900688
+    // award: #05b8cd
+    // event: #19b609
+    private String color;
+
+    //fa-user
+    //university
+    //picture-o
+    //trophy
+    //calendar
+    private String icon;
+
+}
